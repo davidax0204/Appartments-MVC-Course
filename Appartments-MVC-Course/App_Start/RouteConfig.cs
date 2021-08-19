e0faaad477d53153c2883ba258988f200fc405cd
davidax0204 <71772975+davidax0204@users.noreply.github.com>
2021-08-19 15:56:34 +0300

    adding map bla bla bla

--- Appartments-MVC-Course/App_Start/RouteConfig.cs
+++ Appartments-MVC-Course/App_Start/RouteConfig.cs
@@ -13,6 +13,8 @@ namespace Appartments_MVC_Course
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
