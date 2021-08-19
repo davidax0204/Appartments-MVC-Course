12dc7c4aac53f428cd0397e7e71efadcc6fa6d75
davidax0204 <71772975+davidax0204@users.noreply.github.com>
2021-08-19 18:55:46 +0300

    1:30 hour

--- Appartments-MVC-Course/App_Start/BundleConfig.cs
+++ Appartments-MVC-Course/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace Appartments_MVC_Course
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 }
---
+++ Appartments-MVC-Course/Controllers/AppartmentsController.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Principal;
+using System.Web.Mvc;
+
+namespace Appartments_MVC_Course.Controllers
+{
+    public class AppartmentsController : Controller
+    {
+        // GET: Appartments
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // Appartments/stam/id?sortBy=fuck
+        [Route("apartments/theroute/best/{id:range(1,5)}/{sortBy}")]
+        public ActionResult stam(int? id,string sortBy)
+        {
+
+            //return Content("Stam actione");
+            //return HttpNotFound();
+            //return RedirectToAction("Index", "Home");
+
+            if (string.IsNullOrEmpty(sortBy))
+            {
+                sortBy = "none";
+            }
+            return Content("Stam action" + id + ", sort By:"+ sortBy);
+        }
+        public ActionResult RealStam()
+        {
+            return View();
+        }
+    }
+}
